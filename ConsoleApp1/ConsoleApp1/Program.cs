12b276ea769bbc6b48558ce1cf8e86876ce8fc9e
Vatan Soni <vatansoni89@gmail.com>
2019-11-08 12:27:48 +0530

    local cmt master1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worldsss!");
+            Console.WriteLine("Hello Worldsss!sdfsdfcs");
         }
     }
 }
